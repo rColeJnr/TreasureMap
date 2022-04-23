ea2f2d14500dad3121459291934e4b8336061f82
rcolejnr <ricardo.m.jnr@gmail.com>
2022-04-24 00:04:35 +0300

    start building map

--- app/src/main/java/com/rick/treasuremap/MapsActivity.kt
+++ app/src/main/java/com/rick/treasuremap/MapsActivity.kt
@@ -1,32 +1,42 @@
 package com.rick.treasuremap
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.rick.treasuremap.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = com.rick.treasuremap.databinding.ActivityMapsBinding.inflate(layoutInflater)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        if (!LocationPermissionHelper.hasLocationPermission(this)) LocationPermissionHelper.requestPermissions(this)
+        if (!LocationPermissionHelper.hasLocationPermission(this)) LocationPermissionHelper.requestPermissions(
+            this
+        )
     }
 
     /**
@@ -39,12 +49,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.uiSettings.isZoomControlsEnabled = true
+
+        prepareMap()
     }
 
     override fun onRequestPermissionsResult(
@@ -59,7 +68,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         else prepareMap()
     }
 
+
+    @SuppressLint("MissingPermission")
     private fun prepareMap() {
-        TODO("Not yet implemented")
+        if (LocationPermissionHelper.hasLocationPermission(this)) {
+            map.isMyLocationEnabled = true
+
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.apply {
+                    lastLocation = location
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
     }
 }
